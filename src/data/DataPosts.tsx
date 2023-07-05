454cf6dcac060e0fa0a05d4fd07a0535d1e7e8d9
Kaique Miranda <exhardkaique@gmail.com>
2023-07-05 15:50:45 -0300

    Feat: Adicionado novo projeto NLW Spacetime Cápsula do tempo

--- src/data/DataPosts.tsx
+++ src/data/DataPosts.tsx
@@ -249,6 +249,27 @@ const dataPosts = [
   },
   {
     id: 11,
+    title: "NLW Spacetime - Cápsula do tempo",
+    description:
+      "Essa aplicação foi desenvolvida durante a NLW SPACETIME realizada pela Rockseat. Foi utilizado Next.js, typescript, tailwind, node.js, fastify, axios e React Native. É uma aplicação na qual você loga com o seu github e consegue fazer publicações de lembranças, sendo textos, imagens ou até videos. Há também uma opção para deixar a publicação pública, caso contrário, só você conseguirá visualizar a sua publicação.",
+    image: "https://raw.githubusercontent.com/KaiqueBM/kaique-portfolio/master/assets/spacetime.png",
+    video: null,
+    github: "https://github.com/KaiqueBM/nlw-spacetime",
+    project: null,
+    tags: [
+      {
+        name: "Next.js",
+      },
+      {
+        name: "Node.js",
+      },
+      {
+        name: "React Native",
+      },
+    ],
+  },
+  {
+    id: 12,
     title: "Catálogo de filmes",
     description:
       "Desenvolvi uma aplicação web de um catálogo de filmes onde você pode ver os filmes mais populares do momento, filtrar por gêneros, pesquisar um filme especifico e entre outras funções. Consumi a api da The Movies DB, uma api bem completa para o que eu queria fazer. Utilizei nesse projeto React, Typescript e Tailwind. A listagem dos filmes é por sua popularidade atualmente. Fiz um esquema de paginação, mostrando 20 filmes por página. Você pode também filtrar pelos gêneros, listando apenas filmes de ação, comédio, e entre outros. Também tem a opção de pesquisar por um filme especifico, podendo até pesquisar por um filme que ainda não lançou. E para finalizar, pode mudar a linguagem do site por meio de um botão na nav.",
@@ -270,7 +291,7 @@ const dataPosts = [
     ],
   },
   {
-    id: 12,
+    id: 13,
     title: "Yappi",
     description:
       "Aplicação criada para a empresa Yappi. Foi feito em Wordpress utilizando HTML, CSS, Javascript, PHP, Bootstrap, jQuery e Ajax. Além do blog, foi desenvolvido uma calculadora de juros composto, precisando criar um back end interno no WP, utilizando o Ajax para realizar a chamada.",
